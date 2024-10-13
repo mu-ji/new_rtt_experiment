447360aecb1a18a0a5b81a08da3e6afd9187d0bf
mu-ji <1142260696@qq.com>
2024-10-13 15:50:09 +0800

    add playground training

--- Generate_train_test_set.py
+++ Generate_train_test_set.py
@@ -5,10 +5,11 @@ import random
 import pandas as pd
 
 packet_number = 30000
-folder_name = 'test_data'
-distance_list = [0,1]
+folder_name = 'Corridor_data'
+distance_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 sample_length = 200
-sample_number_per_distance = 20
+sample_number_per_distance = 40
+sample_number_per_distance_test = 10
 
 def generate_samples(start_index, rtt_data, rssi_data, sample_length):
     rtt_data = rtt_data[start_index:start_index+sample_length]
@@ -54,19 +55,46 @@ def generate_train_set(folder_name, distance_list, sample_length, sample_number_
     for distance in distance_list:
         distance_data = np.load('{}/{}_distance_{}.npz'.format(folder_name, folder_name, distance))
         distance_rtt_data = distance_data['rtt_data']
-        distance_rtt_data -= 8070
+        distance_rtt_data -= 8073
         for k in range(sample_number_per_distance):
-            start_index = random.randint(20000, 30000-sample_length)
-            sample = generate_samples(start_index, distance_rtt_data, data['rssi_data'], sample_length)
+            start_index = random.randint(10000, 30000-sample_length)
+            sample = generate_samples(start_index, distance_rtt_data, distance_data['rssi_data'], sample_length)
             train_set = update_train_set(train_set, sample, distance)
     return train_set
 
-data = np.load('test_data/test_data_distance_0.npz')
-sample = generate_samples(2, data['rtt_data'], data['rssi_data'], 20)
-print(sample)
+def generate_test_set(folder_name, distance_list, sample_length, sample_number_per_distance_test):
+    test_set = {
+        'rtt_mean': [],
+        'rtt_var': [],
+        'rssi_mean': [],
+        'rssi_var': [],
+        'gmm_mean_1': [],
+        'gmm_mean_2': [],
+        'gmm_var_1': [],
+        'gmm_var_2': [],
+        'gmm_weight_1': [],
+        'gmm_weight_2': [],
+        'label': []
+    }
+    for distance in distance_list:
+        distance_data = np.load('{}/{}_distance_{}.npz'.format(folder_name, folder_name, distance))
+        distance_rtt_data = distance_data['rtt_data']
+        distance_rtt_data -= 8073
+        for k in range(sample_number_per_distance_test):
+            start_index = random.randint(0, 10000-sample_length)
+            sample = generate_samples(start_index, distance_rtt_data, distance_data['rssi_data'], sample_length)
+            test_set = update_train_set(test_set, sample, distance)
+    return test_set
+
+#data = np.load('test_data/test_data_distance_0.npz')
+#sample = generate_samples(2, data['rtt_data'], data['rssi_data'], 20)
+#print(sample)
 
 train_set = generate_train_set(folder_name, distance_list, sample_length, sample_number_per_distance)
+test_set = generate_test_set(folder_name, distance_list, sample_length, sample_number_per_distance_test)
 print(train_set)
 train_set = pd.DataFrame(train_set)
+test_set = pd.DataFrame(test_set)
 train_set.to_csv('train_set/{}_{}_train_set.csv'.format(folder_name, sample_length), index=False)
+test_set.to_csv('test_set/{}_{}_test_set.csv'.format(folder_name, sample_length), index=False)
 print(train_set)
